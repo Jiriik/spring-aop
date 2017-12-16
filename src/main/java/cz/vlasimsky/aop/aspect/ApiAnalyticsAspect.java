ab02434a8ede2595f262d8b34eb57a589ce300c0
jiriik <vlasimsky.jiri@gmail.com>
2017-12-16 20:15:19 +0100

    aspects ordering

--- src/main/java/cz/vlasimsky/aop/aspect/ApiAnalyticsAspect.java
+++ src/main/java/cz/vlasimsky/aop/aspect/ApiAnalyticsAspect.java
@@ -2,10 +2,12 @@ package cz.vlasimsky.aop.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
+@Order(2)
 public class ApiAnalyticsAspect {
 
     @Before("cz.vlasimsky.aop.aspect.AopExpressions.forDaoPackageNoGetSetMethods()")
--- src/main/java/cz/vlasimsky/aop/aspect/LoggingAspect.java
+++ src/main/java/cz/vlasimsky/aop/aspect/LoggingAspect.java
@@ -2,10 +2,12 @@ package cz.vlasimsky.aop.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
+@Order(3)
 public class LoggingAspect {
     // advices for logging
 
